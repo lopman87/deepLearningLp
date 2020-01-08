46e5a39e8067b66a469621d93174ec4c5908adc9
longpeng <longpeng@lattebank.com>
2020-01-08 18:16:21 +0800

    add code 2020年01月08日18:16:17

--- src/main/java/org/deeplearning4j/DownloaderUtility.java
+++ src/main/java/org/deeplearning4j/DownloaderUtility.java
@@ -38,11 +38,36 @@ import java.net.URL;
 public enum DownloaderUtility {
 
     /*
+      Skymind datavec resources stored under AZURE_BLOB_URL/datavec-examples
+   */
+    BASICDATAVECEXAMPLE("BasicDataVecExample.zip","datavec-examples", "92f87e0ceb81093ff8b49e2b4e0a5a02", "1KB"),
+    INPUTSPLIT("inputsplit.zip","datavec-examples", "f316b5274bab3b0f568eded9bee1c67f", "128KB"),
+    IRISDATA("IrisData.zip","datavec-examples","bb49e38bb91089634d7ef37ad8e430b8","1KB"),
+    JOINEXAMPLE("JoinExample.zip","datavec-examples", "cbd6232cf1463d68ff24807d5dd8b530", "1KB"),
+    /*
         Skymind dl4j-examples resources stored under AZURE_BLOB_URL/dl4j-examples
      */
-    IRISDATA("IrisData.zip","datavec-examples","bb49e38bb91089634d7ef37ad8e430b8","1KB"),
-    DATAEXAMPLES("DataExamples.zip","dl4j-examples","e4de9c6f19aaae21fed45bfe2a730cbb","2MB");
+    ANIMALS("animals.zip","dl4j-examples", "1976a1f2b61191d2906e4f615246d63e", "820KB"),
+    ANOMALYSEQUENCEDATA("anomalysequencedata.zip","dl4j-examples", "51bb7c50e265edec3a241a2d7cce0e73", "3MB"),
+    CAPTCHAIMAGE("captchaImage.zip","dl4j-examples","1d159c9587fdbb1cbfd66f0d62380e61","42MB"),
+    CLASSIFICATIONDATA("classification.zip","dl4j-examples","dba31e5838fe15993579edbf1c60c355","77KB"),
+    DATAEXAMPLES("DataExamples.zip","dl4j-examples","e4de9c6f19aaae21fed45bfe2a730cbb","2MB"),
+    LOTTERYDATA("lottery.zip","dl4j-examples","1e54ac1210e39c948aa55417efee193a","2MB"),
+    MODELIMPORT("modelimport.zip","dl4j-examples","411df05aace1c9ff587e430a662ce621","3MB"),
+    NEWSDATA("NewsData.zip","dl4j-examples","0d08e902faabe6b8bfe5ecdd78af9f64","21MB"),
+    NLPDATA("nlp.zip","dl4j-examples","1ac7cd7ca08f13402f0e3b83e20c0512","91MB"),
+    PREDICTGENDERDATA("PredictGender.zip","dl4j-examples","42a3fec42afa798217e0b8687667257e","3MB"),
+    STYLETRANSFER("styletransfer.zip","dl4j-examples","b2b90834d667679d7ee3dfb1f40abe94","3MB"),
 
+    /*
+        Skymind tf-import-examples resources stored under AZURE_BLOB_URL/tf-import-examples
+     */
+    TFIMPORTEXAMPLES("resources.zip","tf-import-examples","4895e40e71b17799e4d6fb75d5a22491","3MB"),
+
+    /*
+        Skymind dl4j-spark example resources stored under AZURE_BLOB_URL/dl4j-spark-examples
+     */
+    PATENTEXAMPLE("patentExample.zip","dl4j-spark-examples","435e2b814d866550678d2ac4d8cc5423","10KB");
     private final String BASE_URL;
     private final String DATA_FOLDER;
     private final String ZIP_FILE;
